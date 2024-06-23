7caadfdb15fda15b197d16b5d6f867afbee3b383
Ryuunotora <144704234+Ryuunotora@users.noreply.github.com>
2024-06-23 11:25:02 -0500

    Inicio

---
+++ DSWII_CL2API_AbelPerez/src/main/java/pe/edu/cibertec/DSWII_CL2API_AbelPerez/DswiiCl2ApiAbelPerezApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL2API_AbelPerez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl2ApiAbelPerezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl2ApiAbelPerezApplication.class, args);
+	}
+
+}
